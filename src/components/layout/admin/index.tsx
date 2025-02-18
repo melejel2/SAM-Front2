c8f6a92d499dab9ec69e0edb8d2025063a0e4620
moaazalsyoufi <alsyoufi2000@gmail.com>
2025-02-18 23:55:48 +0200

    add and control dashboard and admin tools switcher

--- src/components/layout/admin/index.tsx
+++ src/components/layout/admin/index.tsx
@@ -1,53 +1,72 @@
-import { Suspense, useEffect } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 import { Drawer } from "@/components/daisyui";
 
 import Leftbar from "@/components/layout/admin/slots/Leftbar";
 import Topbar from "@/components/layout/admin/slots/Topbar";
-import { adminMenuItems } from "@/data/layout/admin";
+import {
+  adminToolsMenuItems,
+  dashboardMenuItems,
+  mobileAdminToolsMenuItems,
+  mobileDashboardMenuItems,
+} from "@/data/layout/admin";
 import { useLayoutContext } from "@/states/layout";
+import { IMenuItem } from "@/types/layout/admin";
 
 const AdminLayout = ({ children }: { children: any }) => {
-    const {
-        state: { leftbar },
-        toggleLeftbarDrawer,
-    } = useLayoutContext();
+  const {
+    state: { leftbar },
+    toggleLeftbarDrawer,
+  } = useLayoutContext();
 
-    const { pathname } = useLocation();
+  const [activeMenuItems, setActiveMenuItems] = useState<IMenuItem[]>([]);
+  const [mobileActiveMenuItems, setMobileActiveMenuItems] = useState<
+    IMenuItem[]
+  >([]);
 
-    useEffect(() => {
-        toggleLeftbarDrawer(false);
-    }, [pathname]);
+  const { pathname } = useLocation();
 
-    return (
-        <>
-            {
-                <div className="size-full">
-                    <div className="flex overflow-hidden">
-                        <div className="block lg:hidden">
-                            <Drawer
-                                open={leftbar.drawerOpen}
-                                onClickOverlay={() => toggleLeftbarDrawer(false)}
-                                className={`z-20 `}
-                                side={<Leftbar menuItems={adminMenuItems} />}></Drawer>
-                        </div>
-                        <div className="hidden lg:block">
-                            <Leftbar menuItems={adminMenuItems} hide={leftbar.hide} />
-                        </div>
-                        <div className="main-wrapper overflow-auto">
-                            <div className="flex h-full flex-col ">
-                                <Topbar />
-                                <div className="content-wrapper">
-                                    <Suspense>{children}</Suspense>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            }
-        </>
+  useEffect(() => {
+    toggleLeftbarDrawer(false);
+    setActiveMenuItems(
+      leftbar.dashboard ? dashboardMenuItems : adminToolsMenuItems
     );
+
+    setMobileActiveMenuItems(
+      leftbar.dashboard ? mobileDashboardMenuItems : mobileAdminToolsMenuItems
+    );
+  }, [pathname, leftbar.dashboard]);
+
+  return (
+    <>
+      {
+        <div className="size-full">
+          <div className="flex overflow-hidden">
+            <div className="block lg:hidden">
+              <Drawer
+                open={leftbar.drawerOpen}
+                onClickOverlay={() => toggleLeftbarDrawer(false)}
+                className={`z-20 `}
+                side={<Leftbar menuItems={activeMenuItems} />}
+              ></Drawer>
+            </div>
+            <div className="hidden lg:block">
+              <Leftbar menuItems={activeMenuItems} hide={leftbar.hide} />
+            </div>
+            <div className="main-wrapper overflow-auto">
+              <div className="flex h-full flex-col ">
+                <Topbar menuItems={mobileActiveMenuItems} />
+                <div className="content-wrapper">
+                  <Suspense>{children}</Suspense>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      }
+    </>
+  );
 };
 
 export default AdminLayout;
--- src/types/layout/admin/leftbar.ts
+++ src/types/layout/admin/leftbar.ts
@@ -1,10 +1,10 @@
 import { IconifyIcon } from "@iconify/react";
 
 export type IMenuItem = {
-    key: string;
-    icon?: IconifyIcon;
-    label: string;
-    isTitle?: boolean;
-    url?: string;
-    children?: IMenuItem[];
+  key: string;
+  icon?: IconifyIcon;
+  label: string;
+  isTitle?: boolean;
+  url?: string;
+  children?: IMenuItem[];
 };
