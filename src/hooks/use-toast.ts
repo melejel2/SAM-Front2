883b76eb45008174884eb979d3953745bdd125bc
moaazalsyoufi <alsyoufi2000@gmail.com>
2025-02-18 21:27:46 +0200

    update toaster notification

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -1,26 +1,41 @@
 import { toast } from "sonner";
 
 const useToast = () => {
-    const show = (message: string) => {
-        toast(message);
-    };
+  // Function to determine position based on screen width
+  const getPosition = () =>
+    window.innerWidth < 768 ? "top-right" : "bottom-right";
 
-    const showSuccess = (message: string) => {
-        toast.success(message);
-    };
+  const show = (message: string) => {
+    toast(message, { position: getPosition() });
+  };
 
-    const showError = (message: string) => {
-        toast.error(message);
-    };
+  const showSuccess = (message: string) => {
+    toast.success(message, { position: getPosition() });
+  };
 
-    const toaster = {
-        show,
-        success: showSuccess,
-        error: showError,
-    };
-    return {
-        toaster,
-    };
+  const showError = (message: string) => {
+    toast.error(message, { position: getPosition() });
+  };
+
+  const showWarning = (message: string) => {
+    toast.warning(message, { position: getPosition() });
+  };
+
+  const showInfo = (message: string) => {
+    toast.info(message, { position: getPosition() });
+  };
+
+  const toaster = {
+    show,
+    success: showSuccess,
+    error: showError,
+    warning: showWarning,
+    info: showInfo,
+  };
+
+  return {
+    toaster,
+  };
 };
 
 export default useToast;
