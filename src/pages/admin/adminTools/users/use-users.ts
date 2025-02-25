d2f6b85c5bdec47a981f5ee3097e26ce2c06fa0f
moaazalsyoufi <alsyoufi2000@gmail.com>
2025-02-25 23:22:58 +0200

    minor update on use users

--- src/pages/admin/adminTools/users/use-users.ts
+++ src/pages/admin/adminTools/users/use-users.ts
@@ -1,92 +1,92 @@
 import { useState } from "react";
 
 const useUsers = () => {
-  const [hasActions, _] = useState<boolean>(true);
+    const [hasActions, _] = useState<boolean>(true);
 
-  const columns = {
-    id: "ID",
-    name: "Name",
-    username: "Username",
-    password: "Password",
-    site_name: "Site Name",
-    type: "Type",
-    last_login: "Last Login",
-  };
-  const tableData = [
-    {
-      id: "1",
-      name: "Ralph BOU JAOUDE",
-      username: "raboujaoude",
-      password: "***",
-      site_name:
-        "Passcam Yaounde, Passcam Douala, Maroua CHR, Centre Hospitalier Y, Garoua CHR, Ngaoundere CHR, Bertoua CHR, CM Office",
-      type: "Operations Manager",
-      last_login: "11/12/2024",
-    },
-    {
-      id: "2",
-      name: "Ralph BOU JAOUDE",
-      username: "raboujaoude",
-      password: "***",
-      site_name:
-        "Passcam Yaounde, Passcam Douala, Maroua CHR, Centre Hospitalier Y, Garoua CHR, Ngaoundere CHR, Bertoua CHR, CM Office",
-      type: "Operations Manager",
-      last_login: "11/12/2024",
-    },
-    {
-      id: "3",
-      name: "Ralph BOU JAOUDE",
-      username: "raboujaoude",
-      password: "***",
-      site_name:
-        "Passcam Yaounde, Passcam Douala, Maroua CHR, Centre Hospitalier Y, Garoua CHR, Ngaoundere CHR, Bertoua CHR, CM Office",
-      type: "Operations Manager",
-      last_login: "11/12/2024",
-    },
-  ];
+    const columns = {
+        id: "ID",
+        name: "Name",
+        username: "Username",
+        password: "Password",
+        site_name: "Site Name",
+        type: "Type",
+        last_login: "Last Login",
+    };
+    const tableData = [
+        {
+            id: "1",
+            name: "Ralph BOU JAOUDE",
+            username: "raboujaoude",
+            password: "***",
+            site_name:
+                "Passcam Yaounde, Passcam Douala, Maroua CHR, Centre Hospitalier Y, Garoua CHR, Ngaoundere CHR, Bertoua CHR, CM Office",
+            type: "Operations Manager",
+            last_login: "11/12/2024",
+        },
+        {
+            id: "2",
+            name: "Ralph BOU JAOUDE",
+            username: "raboujaoude",
+            password: "***",
+            site_name:
+                "Passcam Yaounde, Passcam Douala, Maroua CHR, Centre Hospitalier Y, Garoua CHR, Ngaoundere CHR, Bertoua CHR, CM Office",
+            type: "Operations Manager",
+            last_login: "11/12/2024",
+        },
+        {
+            id: "3",
+            name: "Ralph BOU JAOUDE",
+            username: "raboujaoude",
+            password: "***",
+            site_name:
+                "Passcam Yaounde, Passcam Douala, Maroua CHR, Centre Hospitalier Y, Garoua CHR, Ngaoundere CHR, Bertoua CHR, CM Office",
+            type: "Operations Manager",
+            last_login: "11/12/2024",
+        },
+    ];
 
-  const inputFields = [
-    {
-      name: "type",
-      label: "Type",
-      type: "select",
-      required: true,
-      options: ["option 1", "option 2", "option 3", "option 4", "option 5"],
-    },
-    {
-      name: "name",
-      label: "Name",
-      type: "text",
-      required: true,
-    },
-    {
-      name: "username",
-      label: "Username",
-      type: "text",
-      required: true,
-    },
-    {
-      name: "password",
-      label: "Password",
-      type: "text",
-      required: true,
-    },
+    const inputFields = [
+        {
+            name: "type",
+            label: "Type",
+            type: "select",
+            required: true,
+            options: ["option 1", "option 2", "option 3", "option 4", "option 5"],
+        },
+        {
+            name: "name",
+            label: "Name",
+            type: "text",
+            required: true,
+        },
+        {
+            name: "username",
+            label: "Username",
+            type: "text",
+            required: true,
+        },
+        {
+            name: "password",
+            label: "Password",
+            type: "text",
+            required: true,
+        },
 
-    {
-      name: "site",
-      label: "Site",
-      type: "select",
-      required: true,
-      options: ["option 1", "option 2", "option 3", "option 4", "option 5"],
-    },
-  ];
+        {
+            name: "site",
+            label: "Site",
+            type: "select",
+            required: true,
+            options: ["option 1", "option 2", "option 3", "option 4", "option 5"],
+        },
+    ];
 
-  return {
-    columns,
-    tableData,
-    inputFields,
-    hasActions,
-  };
+    return {
+        columns,
+        tableData,
+        inputFields,
+        hasActions,
+    };
 };
 
 export default useUsers;
