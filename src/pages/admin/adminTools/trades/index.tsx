022b988ce800f955382c12a0d1a247c1105d9b0e
moaazalsyoufi <alsyoufi2000@gmail.com>
2025-02-25 23:22:24 +0200

    create trades page files

---
+++ src/pages/admin/adminTools/trades/index.tsx
@@ -0,0 +1,30 @@
+import Icon from "@/components/Icon";
+import { MetaData } from "@/components/MetaData";
+import { PageTitle } from "@/components/PageTitle";
+import SAMTable from "@/components/Table";
+
+import useTrades from "./use-trades";
+
+const Trades = () => {
+    const { columns, tableData, inputFields, hasActions } = useTrades();
+
+    return (
+        <div>
+            <MetaData title={"Trades"} />
+
+            <PageTitle title={"Trades"} centerItem={"Admin tools"} />
+            <div>
+                <SAMTable
+                    columns={columns}
+                    tableData={tableData}
+                    inputFields={inputFields}
+                    actions={hasActions}
+                    title={"Trades"}
+                    loading={false}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default Trades;
