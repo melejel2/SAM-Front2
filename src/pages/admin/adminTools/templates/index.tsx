80ed921bff2f414a1152f8132b383c5cc6212b16
moaazalsyoufi <alsyoufi2000@gmail.com>
2025-02-25 23:22:15 +0200

    create templates page files

---
+++ src/pages/admin/adminTools/templates/index.tsx
@@ -0,0 +1,30 @@
+import Icon from "@/components/Icon";
+import { MetaData } from "@/components/MetaData";
+import { PageTitle } from "@/components/PageTitle";
+import SAMTable from "@/components/Table";
+
+import useTemplates from "./use-templates";
+
+const Templates = () => {
+    const { columns, tableData, inputFields, hasActions } = useTemplates();
+
+    return (
+        <div>
+            <MetaData title={"Templates"} />
+
+            <PageTitle title={"Templates"} centerItem={"Admin tools"} />
+            <div>
+                <SAMTable
+                    columns={columns}
+                    tableData={tableData}
+                    inputFields={inputFields}
+                    actions={hasActions}
+                    title={"Templates"}
+                    loading={false}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default Templates;
