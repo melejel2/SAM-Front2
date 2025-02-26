ecec42358d2db06b8a534fab93c3048d681cea18
moaazalsyoufi <alsyoufi2000@gmail.com>
2025-02-26 09:58:27 +0200

    create Reports page files

---
+++ src/pages/admin/dashboards/reports/index.tsx
@@ -0,0 +1,32 @@
+import { MetaData } from "@/components/MetaData";
+import { PageTitle } from "@/components/PageTitle";
+import SAMTable from "@/components/Table";
+
+import useReports from "./use-reports";
+
+const Reports = () => {
+    const { columns, tableData, inputFields } = useReports();
+
+    return (
+        <div>
+            <MetaData title={"Reports"} />
+
+            <PageTitle title={"Reports"} centerItem={"Dashboard"} />
+            <div>
+                <SAMTable
+                    columns={columns}
+                    tableData={tableData}
+                    inputFields={inputFields}
+                    actions={true}
+                    editAction={true}
+                    deleteAction={true}
+                    title={"Report"}
+                    loading={false}
+                    addBtn={true}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default Reports;
