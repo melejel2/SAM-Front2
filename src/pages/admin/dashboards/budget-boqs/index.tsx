cf27b7db0ce6924035d4a41e795c68b7d25f6bc7
moaazalsyoufi <alsyoufi2000@gmail.com>
2025-02-26 09:47:22 +0200

    create budget BOQs page files

---
+++ src/pages/admin/dashboards/budget-boqs/index.tsx
@@ -0,0 +1,32 @@
+import { MetaData } from "@/components/MetaData";
+import { PageTitle } from "@/components/PageTitle";
+import SAMTable from "@/components/Table";
+
+import useBudgetBOQs from "./use-budget-boqs";
+
+const BudgetBOQs = () => {
+    const { columns, tableData, inputFields } = useBudgetBOQs();
+
+    return (
+        <div>
+            <MetaData title={"Budget BOQs"} />
+
+            <PageTitle title={"Budget BOQs"} centerItem={"Dashboard"} />
+            <div>
+                <SAMTable
+                    columns={columns}
+                    tableData={tableData}
+                    inputFields={inputFields}
+                    actions={true}
+                    editAction={true}
+                    deleteAction={true}
+                    title={"Budget BOQs"}
+                    loading={false}
+                    addBtn={true}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default BudgetBOQs;
---
+++ src/pages/admin/dashboards/budget-boqs/use-budget-boqs.ts
@@ -0,0 +1,105 @@
+const useBudgetBOQs = () => {
+    const columns = {
+        first_name: "First Name",
+        last_name: "Last Name",
+        telephone: "Telephone",
+        email: "Email",
+        username: "Username",
+        password: "Password",
+        role: "Role",
+    };
+    const tableData = [
+        {
+            id: "1",
+            first_name: "First Name 1",
+            last_name: "Last Name 1",
+            telephone: "Telephone 1",
+            email: "Email 1",
+            username: "Username 1",
+            password: "Password 1",
+            role: "Role 1",
+        },
+        {
+            id: "2",
+            first_name: "First Name 2",
+            last_name: "Last Name 2",
+            telephone: "Telephone 2",
+            email: "Email 2",
+            username: "Username 2",
+            password: "Password 2",
+            role: "Role 2",
+        },
+        {
+            id: "3",
+            first_name: "First Name 3",
+            last_name: "Last Name 3",
+            telephone: "Telephone 3",
+            email: "Email 3",
+            username: "Username 3",
+            password: "Password 3",
+            role: "Role 3",
+        },
+    ];
+
+    const inputFields = [
+        {
+            name: "first_name",
+            label: "First Name",
+            type: "text",
+            required: true,
+        },
+        {
+            name: "last_name",
+            label: "Last Name",
+            type: "text",
+            required: true,
+        },
+        {
+            name: "telephone",
+            label: "Telephone",
+            type: "text",
+            required: true,
+        },
+        {
+            name: "email",
+            label: "Email",
+            type: "email",
+            required: true,
+        },
+        {
+            name: "username",
+            label: "Username",
+            type: "text",
+            required: true,
+        },
+        {
+            name: "password",
+            label: "Password",
+            type: "text",
+            required: true,
+        },
+
+        {
+            name: "role",
+            label: "Role",
+            type: "select",
+            required: true,
+            options: [
+                "Regional Operations Manager",
+                "General Manager",
+                "Operations Manager",
+                "Contracts Manager",
+                "Accountant",
+                "Admin",
+            ],
+        },
+    ];
+
+    return {
+        columns,
+        tableData,
+        inputFields,
+    };
+};
+
+export default useBudgetBOQs;
