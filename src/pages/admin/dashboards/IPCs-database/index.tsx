e9a1eaeff80273230e2a36d006703e614afa4515
moaazalsyoufi <alsyoufi2000@gmail.com>
2025-02-26 09:56:33 +0200

    create IPCs database page files

---
+++ src/pages/admin/dashboards/IPCs-database/index.tsx
@@ -0,0 +1,32 @@
+import { MetaData } from "@/components/MetaData";
+import { PageTitle } from "@/components/PageTitle";
+import SAMTable from "@/components/Table";
+
+import useIPCsDatabase from "./use-IPCs-database";
+
+const IPCsDatabase = () => {
+    const { columns, tableData, inputFields } = useIPCsDatabase();
+
+    return (
+        <div>
+            <MetaData title={"IPCs Database"} />
+
+            <PageTitle title={"IPCs Database"} centerItem={"Dashboard"} />
+            <div>
+                <SAMTable
+                    columns={columns}
+                    tableData={tableData}
+                    inputFields={inputFields}
+                    actions={true}
+                    editAction={true}
+                    deleteAction={true}
+                    title={"IPC"}
+                    loading={false}
+                    addBtn={true}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default IPCsDatabase;
