acd623cf81d2a8a5b4dd6f1355bc946a424b8b85
moaazalsyoufi <alsyoufi2000@gmail.com>
2025-02-26 07:51:34 +0200

    update currencies and units table, add, and edit

--- src/pages/admin/adminTools/currencies-and-units/index.tsx
+++ src/pages/admin/adminTools/currencies-and-units/index.tsx
@@ -6,22 +6,48 @@ import SAMTable from "@/components/Table";
 import useCurrenciesAndUnits from "./use-currencies-and-units";
 
 const CurrenciesAndUnits = () => {
-    const { columns, tableData, inputFields, hasActions } = useCurrenciesAndUnits();
+    const {
+        currenciesColumns,
+        currencyTableData,
+        currencyInputFields,
+        unitsColumns,
+        unitsTableData,
+        unitInputFields,
+        hasActions,
+    } = useCurrenciesAndUnits();
 
     return (
         <div>
             <MetaData title={"Currencies And Units"} />
 
             <PageTitle title={"Currencies And Units"} centerItem={"Admin tools"} />
-            <div>
-                <SAMTable
-                    columns={columns}
-                    tableData={tableData}
-                    inputFields={inputFields}
-                    actions={hasActions}
-                    title={"Currencies And Units"}
-                    loading={false}
-                />
+            <div className="flex flex-col justify-between space-y-6 xl:flex-row xl:space-y-0 xl:space-x-6">
+                <div className="w-full xl:w-1/2">
+                    <SAMTable
+                        columns={currenciesColumns}
+                        tableData={currencyTableData}
+                        inputFields={currencyInputFields}
+                        actions={true}
+                        deleteAction={true}
+                        editAction={true}
+                        title={"Currency"}
+                        loading={false}
+                        addBtn={true}
+                    />
+                </div>
+                <div className="w-full xl:w-1/2">
+                    <SAMTable
+                        columns={unitsColumns}
+                        tableData={unitsTableData}
+                        inputFields={unitInputFields}
+                        actions={true}
+                        deleteAction={true}
+                        editAction={true}
+                        title={"Unit"}
+                        loading={false}
+                        addBtn={true}
+                    />
+                </div>
             </div>
         </div>
     );
--- src/pages/admin/adminTools/currencies-and-units/use-currencies-and-units.ts
+++ src/pages/admin/adminTools/currencies-and-units/use-currencies-and-units.ts
@@ -3,88 +3,108 @@ import { useState } from "react";
 const useCurrenciesAndUnits = () => {
     const [hasActions, _] = useState<boolean>(true);
 
-    const columns = {
-        id: "ID",
-        name: "Name",
-        username: "Username",
-        password: "Password",
-        site_name: "Site Name",
-        type: "Type",
-        last_login: "Last Login",
+    const currenciesColumns = {
+        currency_code: "Currency Code",
+        currency_full_name: "Currency Full Name",
+        usd_equivalency: "USD Equivalency",
     };
-    const tableData = [
+
+    const unitsColumns = {
+        unit_list: "Unit List",
+    };
+
+    const currencyTableData = [
         {
             id: "1",
-            name: "Ralph BOU JAOUDE",
-            username: "raboujaoude",
-            password: "***",
-            site_name:
-                "Passcam Yaounde, Passcam Douala, Maroua CHR, Centre Hospitalier Y, Garoua CHR, Ngaoundere CHR, Bertoua CHR, CM Office",
-            type: "Operations Manager",
-            last_login: "11/12/2024",
+            currency_code: "MAD",
+            currency_full_name: "Moroccan Dirham",
+            usd_equivalency: "8.973139",
         },
         {
             id: "2",
-            name: "Ralph BOU JAOUDE",
-            username: "raboujaoude",
-            password: "***",
-            site_name:
-                "Passcam Yaounde, Passcam Douala, Maroua CHR, Centre Hospitalier Y, Garoua CHR, Ngaoundere CHR, Bertoua CHR, CM Office",
-            type: "Operations Manager",
-            last_login: "11/12/2024",
+            currency_code: "EUR",
+            currency_full_name: "Euro",
+            usd_equivalency: "0.830802",
         },
         {
             id: "3",
-            name: "Ralph BOU JAOUDE",
-            username: "raboujaoude",
-            password: "***",
-            site_name:
-                "Passcam Yaounde, Passcam Douala, Maroua CHR, Centre Hospitalier Y, Garoua CHR, Ngaoundere CHR, Bertoua CHR, CM Office",
-            type: "Operations Manager",
-            last_login: "11/12/2024",
+            currency_code: "USD",
+            currency_full_name: "United States Dollar",
+            usd_equivalency: "1",
+        },
+        {
+            id: "4",
+            currency_code: "XAF",
+            currency_full_name: "Central African CFA Franc",
+            usd_equivalency: "544.475617",
+        },
+        {
+            id: "5",
+            currency_code: "XOF",
+            currency_full_name: "West African CFA Franc",
+            usd_equivalency: "544.475617",
+        },
+        {
+            id: "6",
+            currency_code: "IQD",
+            currency_full_name: "Iraqi Dinar",
+            usd_equivalency: "1457.065312",
         },
     ];
 
-    const inputFields = [
-        {
-            name: "type",
-            label: "Type",
-            type: "select",
-            required: true,
-            options: ["option 1", "option 2", "option 3", "option 4", "option 5"],
-        },
-        {
-            name: "name",
-            label: "Name",
-            type: "text",
-            required: true,
-        },
-        {
-            name: "username",
-            label: "Username",
-            type: "text",
-            required: true,
-        },
-        {
-            name: "password",
-            label: "Password",
-            type: "text",
-            required: true,
-        },
+    const unitsTableData = [
+        { id: "1", unit_list: "ft" },
+        { id: "2", unit_list: "U" },
+        { id: "3", unit_list: "D" },
+        { id: "4", unit_list: "dm" },
+        { id: "5", unit_list: "ens" },
+        { id: "6", unit_list: "ml" },
+        { id: "7", unit_list: "m2" },
+        { id: "8", unit_list: "m3" },
+        { id: "9", unit_list: "kg" },
+        { id: "10", unit_list: "ton" },
+        { id: "11", unit_list: "liter" },
+        { id: "12", unit_list: "mois" },
+        { id: "13", unit_list: "jour" },
+    ];
 
+    const currencyInputFields = [
         {
-            name: "site",
-            label: "Site",
-            type: "select",
+            name: "currency_code",
+            label: "Currency Code",
+            type: "text",
             required: true,
-            options: ["option 1", "option 2", "option 3", "option 4", "option 5"],
+        },
+        {
+            name: "currency_full_name",
+            label: "Currency Full Name",
+            type: "text",
+            required: true,
+        },
+        {
+            name: "usd_equivalency",
+            label: "USD Equivalency",
+            type: "number",
+            required: true,
+        },
+    ];
+
+    const unitInputFields = [
+        {
+            name: "unit_list",
+            label: "Unit List",
+            type: "text",
+            required: true,
         },
     ];
 
     return {
-        columns,
-        tableData,
-        inputFields,
+        currenciesColumns,
+        unitsColumns,
+        currencyTableData,
+        unitsTableData,
+        currencyInputFields,
+        unitInputFields,
         hasActions,
     };
 };
