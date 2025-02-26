15ce5a2c59a166fa012db73e0fa17b9fd0202de1
moaazalsyoufi <alsyoufi2000@gmail.com>
2025-02-26 09:49:56 +0200

    create contracts database page files

---
+++ src/pages/admin/dashboards/contracts-database/index.tsx
@@ -0,0 +1,32 @@
+import { MetaData } from "@/components/MetaData";
+import { PageTitle } from "@/components/PageTitle";
+import SAMTable from "@/components/Table";
+
+import useContractsDatabase from "./use-contracts-database";
+
+const ContractsDatabase = () => {
+    const { columns, tableData, inputFields } = useContractsDatabase();
+
+    return (
+        <div>
+            <MetaData title={"Contracts Database"} />
+
+            <PageTitle title={"Contracts Database"} centerItem={"Dashboard"} />
+            <div>
+                <SAMTable
+                    columns={columns}
+                    tableData={tableData}
+                    inputFields={inputFields}
+                    actions={true}
+                    editAction={true}
+                    deleteAction={true}
+                    title={"Contract"}
+                    loading={false}
+                    addBtn={true}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default ContractsDatabase;
