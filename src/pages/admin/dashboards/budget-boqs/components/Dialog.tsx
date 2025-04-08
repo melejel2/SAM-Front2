3908b3ae83d4a64626598d4ca2eec36fcc781395
moaazalsyoufi <alsyoufi2000@gmail.com>
2025-04-08 19:03:15 +0300

    update BOQ dialog UI

--- src/pages/admin/dashboards/budget-boqs/components/Dialog.tsx
+++ src/pages/admin/dashboards/budget-boqs/components/Dialog.tsx
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 import { Button } from "@/components/daisyui";
 import useToast from "@/hooks/use-toast";
 
+import ProjectStep from "./ProjectStep";
+
 interface BOQDialogProps {
     handleHide: () => void;
     dialogRef: React.RefObject<HTMLDialogElement | null>;
@@ -12,8 +14,19 @@ interface BOQDialogProps {
 
 const BOQDialogComponent: React.FC<BOQDialogProps> = ({ handleHide, dialogRef, dialogType, onSuccess }) => {
     const [isLoading, setIsLoading] = useState(false);
+    const [currentStep, setCurrentStep] = useState(0);
 
     const { toaster } = useToast();
+
+    const steps = [
+        { label: "Project", content: <ProjectStep /> },
+        { label: "Trade", content: <div>Trade content here</div> },
+        { label: "Buildings", content: <div>Buildings content here</div> },
+        { label: "Subcontractor", content: <div>Subcontractor content here</div> },
+        { label: "Particular Conditions", content: <div>Particular Conditions content here</div> },
+        { label: "BOQ", content: <div>BOQ content here</div> },
+        { label: "Preview", content: <div>Preview content here</div> },
+    ];
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -32,18 +45,16 @@ const BOQDialogComponent: React.FC<BOQDialogProps> = ({ handleHide, dialogRef, d
 
     return (
         <dialog ref={dialogRef as React.Ref<HTMLDialogElement>} className="modal" aria-modal="true">
-            <div className="modal-box relative max-w-7xl">
-                <form onSubmit={handleSubmit} className="space-y-4">
+            <div className="modal-box relative h-[85%] max-w-[85%]">
+                <form onSubmit={handleSubmit} className="h-[84%] space-y-4">
                     <div>
                         <div className="text-center">
-                            <ul className="steps text-sm">
-                                <li className="step step-primary">Project</li>
-                                <li className="step">Trade</li>
-                                <li className="step">Buildings</li>
-                                <li className="step">Subcontractor</li>
-                                <li className="step">Particular Conditions</li>
-                                <li className="step">BOQ</li>
-                                <li className="step">Preview</li>
+                            <ul className="steps overflow-x-auto text-sm">
+                                {steps.map((step, index) => (
+                                    <li key={index} className={`step ${index <= currentStep ? "step-primary" : ""}`}>
+                                        {step.label}
+                                    </li>
+                                ))}
                             </ul>
                         </div>
                         <button
@@ -54,17 +65,53 @@ const BOQDialogComponent: React.FC<BOQDialogProps> = ({ handleHide, dialogRef, d
                             ✕
                         </button>
                     </div>
-                    <div>content here</div>
-                    <div className="">
+                    <div className="mt-4 flex h-full items-center justify-between">
                         <Button
-                            className="mt-2 w-full"
-                            size="sm"
-                            type="submit"
-                            disabled={isLoading}
-                            loading={isLoading}>
-                            {dialogType === "Add" ? "Add" : "Save"}
+                            type="button"
+                            color="ghost"
+                            className="btn-circle"
+                            disabled={currentStep === 0}
+                            onClick={() => setCurrentStep((prev) => prev - 1)}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+                                <g
+                                    fill="none"
+                                    stroke="currentColor"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2">
+                                    <circle cx="12" cy="12" r="10" />
+                                    <path d="M16 12H8m4-4l-4 4l4 4" />
+                                </g>
+                            </svg>
+                        </Button>
+
+                        {/* Content */}
+                        <div className="h-full px-2">{steps[currentStep].content}</div>
+
+                        <Button
+                            type="button"
+                            color="ghost"
+                            className="btn-circle"
+                            disabled={currentStep === steps.length - 1}
+                            onClick={() => setCurrentStep((prev) => prev + 1)}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+                                <g
+                                    fill="none"
+                                    stroke="currentColor"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2">
+                                    <circle cx="12" cy="12" r="10" />
+                                    <path d="M8 12h8m-4 4l4-4l-4-4" />
+                                </g>
+                            </svg>
                         </Button>
                     </div>
+                    {currentStep === steps.length - 1 && (
+                        <Button className="w-full" size="sm" type="submit" disabled={isLoading} loading={isLoading}>
+                            {dialogType === "Add" ? "Add" : "Save"}
+                        </Button>
+                    )}
                 </form>
             </div>
         </dialog>
