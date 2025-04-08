a8cc4bbb489aca67c8b0d5c82270eeafd679cb2e
moaazalsyoufi <alsyoufi2000@gmail.com>
2025-04-08 17:17:45 +0300

    create static BOQ dialog component

---
+++ src/pages/admin/dashboards/budget-boqs/components/Dialog.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+
+import { Button } from "@/components/daisyui";
+import useToast from "@/hooks/use-toast";
+
+interface BOQDialogProps {
+    handleHide: () => void;
+    dialogRef: React.RefObject<HTMLDialogElement | null>;
+    dialogType: "Add" | "Edit" | "Preview";
+    onSuccess: () => void;
+}
+
+const BOQDialogComponent: React.FC<BOQDialogProps> = ({ handleHide, dialogRef, dialogType, onSuccess }) => {
+    const [isLoading, setIsLoading] = useState(false);
+
+    const { toaster } = useToast();
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setIsLoading(true);
+
+        setIsLoading(false);
+
+        toaster.success("Done...");
+        onSuccess();
+        handleClose();
+    };
+
+    const handleClose = () => {
+        handleHide();
+    };
+
+    return (
+        <dialog ref={dialogRef as React.Ref<HTMLDialogElement>} className="modal" aria-modal="true">
+            <div className="modal-box relative max-w-7xl">
+                <form onSubmit={handleSubmit} className="space-y-4">
+                    <div>
+                        <button
+                            type="button"
+                            className="btn btn-sm btn-circle btn-ghost absolute top-2 right-2"
+                            onClick={handleClose}
+                            aria-label="Close">
+                            ✕
+                        </button>
+                    </div>
+                    <div>content here</div>
+                    <div className="">
+                        <Button
+                            className="mt-2 w-full"
+                            size="sm"
+                            type="submit"
+                            disabled={isLoading}
+                            loading={isLoading}>
+                            {dialogType === "Add" ? "Add" : "Save"}
+                        </Button>
+                    </div>
+                </form>
+            </div>
+        </dialog>
+    );
+};
+
+export default BOQDialogComponent;
