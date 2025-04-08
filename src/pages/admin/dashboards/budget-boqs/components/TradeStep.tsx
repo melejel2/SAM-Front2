a7c8f2601c11548a2c4b8e3580a60db930eb3461
moaazalsyoufi <alsyoufi2000@gmail.com>
2025-04-08 19:10:37 +0300

    create all boq steps components

---
+++ src/pages/admin/dashboards/budget-boqs/components/TradeStep.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function TradeStep() {
+    return (
+        <div>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint ducimus doloribus reprehenderit qui ut
+            exercitationem nulla ipsam eligendi tenetur! Sed, quis eos! Corporis tempore est amet sequi. Impedit,
+            possimus voluptates. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint ducimus doloribus
+            reprehenderit qui ut exercitationem nulla ipsam eligendi tenetur! Sed, quis eos! Corporis tempore est amet
+            sequi. Impedit, possimus voluptates. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint ducimus
+            doloribus reprehenderit qui ut exercitationem nulla ipsam eligendi tenetur! Sed, quis eos! Corporis tempore
+            est amet sequi. Impedit, possimus voluptates. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint
+            ducimus doloribus reprehenderit qui ut exercitationem nulla ipsam eligendi tenetur! Sed, quis eos! Corporis
+            tempore est amet sequi. Impedit, possimus voluptates.
+        </div>
+    );
+}
+
+export default TradeStep;
