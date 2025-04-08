4296b7694abd25e6c040b06a3a0994f93069a368
moaazalsyoufi <alsyoufi2000@gmail.com>
2025-04-08 17:18:53 +0300

    open static create and update budget BOQ from table

--- src/pages/admin/dashboards/budget-boqs/index.tsx
+++ src/pages/admin/dashboards/budget-boqs/index.tsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
+
 import { MetaData } from "@/components/MetaData";
 import { PageTitle } from "@/components/PageTitle";
 import SAMTable from "@/components/Table";
+import { useDialog } from "@/components/daisyui";
 
+import BOQDialogComponent from "./components/Dialog";
 import useBudgetBOQs from "./use-budget-BOQs";
 
 const BudgetBOQs = () => {
+    const [dialogType, setDialogType] = useState<"Add" | "Edit" | "Delete" | "Preview">("Add");
+
     const { columns, tableData, inputFields } = useBudgetBOQs();
+    const { dialogRef, handleShow, handleHide } = useDialog();
+
+    const openCreateDialog = async (type: string) => {};
 
     return (
         <div>
@@ -24,8 +33,12 @@ const BudgetBOQs = () => {
                     loading={false}
                     addBtn={true}
                     onSuccess={() => {}}
+                    dynamicDialog={false}
+                    openStaticDialog={handleShow}
                 />
             </div>
+
+            <BOQDialogComponent handleHide={handleHide} dialogRef={dialogRef} dialogType={"Add"} onSuccess={() => {}} />
         </div>
     );
 };
