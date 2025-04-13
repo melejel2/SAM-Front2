257a3ade79d2ffa3e2ecf93a42a3d3d76140e0ba
moaazalsyoufi <alsyoufi2000@gmail.com>
2025-04-14 00:28:40 +0300

    define budget boq table columns with dummy data

--- src/pages/admin/dashboards/budget-boqs/components/use-budget-boq-dialog.ts
+++ src/pages/admin/dashboards/budget-boqs/components/use-budget-boq-dialog.ts
@@ -1,15 +1,53 @@
 import { useState } from "react";
 
 const useBudgetBOQsDialog = () => {
-    const [selectedProject, setSelectedProject] = useState<any>(null);
     const [selectedTrade, setSelectedTrade] = useState<any>(null);
     const [selectedBuilding, setSelectedBuilding] = useState<any>(null);
 
+    const columns = {
+        nb: "N°",
+        item: "Item",
+        unit: "Unit",
+        qty: "Quantity",
+        unit_price: "Unit Price",
+        total_price: "Total Price",
+    };
+
+    const tableData = [
+        {
+            id: 1,
+            nb: 1,
+            item: "item 1",
+            unit: "unit 1",
+            qty: 1,
+            unit_price: 2,
+            total_price: 2,
+        },
+        {
+            id: 2,
+            nb: 2,
+            item: "item 2",
+            unit: "unit 2",
+            qty: 2,
+            unit_price: 3,
+            total_price: 6,
+        },
+        {
+            id: 3,
+            nb: 3,
+            item: "item 3",
+            unit: "unit 3",
+            qty: 4,
+            unit_price: 5,
+            total_price: 20,
+        },
+    ];
+
     return {
-        selectedProject,
+        columns,
+        tableData,
         selectedTrade,
         selectedBuilding,
-        setSelectedProject,
         setSelectedTrade,
         setSelectedBuilding,
     };
