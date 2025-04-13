b3f1bcf00c4df35c04c00077705628746170b566
moaazalsyoufi <alsyoufi2000@gmail.com>
2025-04-14 00:30:55 +0300

    get and display budget BOQ table data on its dialog

--- src/pages/admin/dashboards/budget-boqs/components/BOQ/index.tsx
+++ src/pages/admin/dashboards/budget-boqs/components/BOQ/index.tsx
@@ -1,17 +1,33 @@
-import React from "react";
+import { useEffect } from "react";
+
+import SAMTable from "@/components/Table";
+import useTrades from "@/pages/admin/adminTools/trades/use-trades";
+
+import useBudgetBOQsDialog from "../use-budget-boq-dialog";
 
 function BOQStep() {
+    const { getTrades, sheets } = useTrades();
+    const { columns, tableData } = useBudgetBOQsDialog();
+
+    useEffect(() => {
+        getTrades();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return (
-        <div>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint ducimus doloribus reprehenderit qui ut
-            exercitationem nulla ipsam eligendi tenetur! Sed, quis eos! Corporis tempore est amet sequi. Impedit,
-            possimus voluptates. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint ducimus doloribus
-            reprehenderit qui ut exercitationem nulla ipsam eligendi tenetur! Sed, quis eos! Corporis tempore est amet
-            sequi. Impedit, possimus voluptates. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint ducimus
-            doloribus reprehenderit qui ut exercitationem nulla ipsam eligendi tenetur! Sed, quis eos! Corporis tempore
-            est amet sequi. Impedit, possimus voluptates. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint
-            ducimus doloribus reprehenderit qui ut exercitationem nulla ipsam eligendi tenetur! Sed, quis eos! Corporis
-            tempore est amet sequi. Impedit, possimus voluptates.
+        <div className="h-full">
+            <SAMTable
+                columns={columns}
+                tableData={tableData}
+                inputFields={[]}
+                title={"Budget BOQ"}
+                loading={false}
+                onSuccess={() => {}}
+                hasSheets={true}
+                sheets={sheets}
+                actions
+                deleteAction
+            />
         </div>
     );
 }
