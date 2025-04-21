84c0d99c7c71b99596d8cedd50fffb3a07c053a3
moaazalsyoufi <alsyoufi2000@gmail.com>
2025-04-22 01:10:57 +0300

    add active view btns to change contacts table data by active type

--- src/pages/admin/dashboards/contracts-database/index.tsx
+++ src/pages/admin/dashboards/contracts-database/index.tsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
+
 import { MetaData } from "@/components/MetaData";
 import { PageTitle } from "@/components/PageTitle";
 import SAMTable from "@/components/Table";
+import { Button } from "@/components/daisyui";
 
 import useContractsDatabase from "./use-contracts-database";
 
 const ContractsDatabase = () => {
+    const [activeView, setActiveView] = useState<"Contract Database" | "VOs Database" | "Terminated Contracts">(
+        "Contract Database",
+    );
+
     const { columns, tableData, inputFields } = useContractsDatabase();
 
     return (
@@ -13,6 +20,29 @@ const ContractsDatabase = () => {
 
             <PageTitle title={"Contracts Database"} centerItem={"Dashboard"} />
             <div>
+                <div className="flex items-center justify-center space-x-6">
+                    <Button
+                        className="disabled:text-base-content disabled:!bg-primary space-x-2"
+                        disabled={activeView === "Contract Database"}
+                        onClick={() => setActiveView("Contract Database")}>
+                        <span className="iconify lucide--file-spreadsheet size-6"></span>
+                        <span>Contract Database</span>
+                    </Button>
+                    <Button
+                        className="disabled:text-base-content disabled:!bg-primary space-x-2"
+                        disabled={activeView === "VOs Database"}
+                        onClick={() => setActiveView("VOs Database")}>
+                        <span className="iconify lucide--file-text size-6"></span>
+                        <span>VOS Database</span>
+                    </Button>
+                    <Button
+                        className="disabled:text-base-content disabled:!bg-primary space-x-2"
+                        disabled={activeView === "Terminated Contracts"}
+                        onClick={() => setActiveView("Terminated Contracts")}>
+                        <span className="iconify lucide--file-x size-6"></span>
+                        <span>Terminated Contracts</span>
+                    </Button>
+                </div>
                 <SAMTable
                     columns={columns}
                     tableData={tableData}
